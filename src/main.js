87b5d906d2e5e906bbaeb6c29a311d9314e9a1a4
iryshanykkolaevna <141450700+iryshanykkolaevna@users.noreply.github.com>
2024-01-26 19:32:22 +0200

    add

--- src/main.js
+++ src/main.js
@@ -1,26 +1,38 @@
+'use strict';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+import axios from 'axios';
 
 const refs = {
   form: document.querySelector('.search-form'),
   input: document.querySelector('.search-inp'),
-  gallery: document.querySelector('.gallery'),
-  loader: document.querySelector('.loader'),
   searchBtn: document.querySelector('.search-btn'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-btn'),
+  loader: document.querySelector('.loader'),
 };
 
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '41899926-74a7536d4d492e936dbb67b5b';
 
-const simplyGallery = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-refs.form.addEventListener('submit', event => {
+const hiddenClass = 'is-hidden';
+let page = 1;
+let query = '';
+let maxPage = 0;
+
+refs.form.addEventListener('submit', onSearch);
+
+async function onSearch(event) {
   event.preventDefault();
-  const query = refs.form.query.value.trim();
+  refs.gallery.innerHTML = '';
+  page = 1;
+  refs.loadMoreBtn.classList.add(hiddenClass);
+  query = refs.form.query.value.trim();
 
   if (!query) {
     createMessage(
@@ -28,38 +40,77 @@ refs.form.addEventListener('submit', event => {
     );
     return;
   }
-  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
-  
-  fetchImages(url)
-    .then(data => {
-      if (data.hits.length === 0) {
-        createMessage(
-          `Sorry, there are no images matching your search query. Please, try again!`
-        );
-        showLoader(false);
-      }
-  
-      refs.gallery.innerHTML = createMarkup(data.hits);
-      simplyGallery.refresh()
-      showLoader(false);
-      
-      refs.form.reset();
-    })
-    .catch(error => console.error(error));
-});
+  try {
+    const { hits, total } = await getImages(query);
+    maxPage = Math.ceil(total / 40);
+    createMarkup(hits, refs.gallery);
 
-function fetchImages(url) {
-  showLoader(true);
-  return fetch(url).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.ststusText);
+    if (hits.length > 0) {
+      refs.loadMoreBtn.classList.remove(hiddenClass);
+      refs.loadMoreBtn.addEventListener('click', onLoadMore);
+    } else {
+      refs.loadMoreBtn.classList.add(hiddenClass);
+      createMessage(
+        `Sorry, there are no images matching your search query. Please, try again!`
+      );
     }
-    return resp.json();
-  });
+    showLoader(false);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    refs.form.reset();
+    if (page === maxPage) {
+      refs.loadMoreBtn.classList.add(hiddenClass);
+      createMessage(
+        "We're sorry, but you've reached the end of search results!"
+      );
+    }
+  }
+}
+
+async function onLoadMore() {
+  page += 1;
+  try {
+    showLoader(true);
+    refs.loadMoreBtn.classList.add(hiddenClass);
+    const { hits } = await getImages(query, page);
+    createMarkup(hits, refs.gallery);
+    showLoader(false);
+
+    scrollImg();
+
+    refs.loadMoreBtn.classList.remove(hiddenClass);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    if (page > maxPage) {
+      refs.loadMoreBtn.classList.add(hiddenClass);
+      createMessage(
+        "We're sorry, but you've reached the end of search results!"
+      );
+    }
+  }
+}
+
+async function getImages(query, page = 1) {
+  showLoader(true);
+  return axios
+    .get('/', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page,
+      },
+    })
+    .then(({ data }) => data);
 }
 
 function createMarkup(hits) {
-  return hits
+  const markup = hits
     .map(
       ({
         webformatURL,
@@ -78,11 +129,16 @@ function createMarkup(hits) {
       src="${webformatURL}"
       alt="${tags}"
     />
-    <p class="gallery-descr">Likes: <span class="descr-span">${likes}</span> Views: <span class="descr-span">${views}</span> Comments: <span class="descr-span">${comments}</span> Downloads: <span class="descr-span">${downloads}</span></p>
+    <p class="gallery-descr">likes: <span class="descr-span">${likes}</span> views: <span class="descr-span">${views}</span> comments: <span class="descr-span">${comments}</span> downloads: <span class="descr-span">${downloads}</span></p>
   </a>
 </li>`
     )
     .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  const simplyGallery = new SimpleLightbox('.gallery-item a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 }
 
 function createMessage(message) {
@@ -102,5 +158,9 @@ function createMessage(message) {
 
 function showLoader(state = true) {
   refs.loader.style.display = !state ? 'none' : 'inline-block';
-  refs.searchBtn.disabled = state;
+}
+
+function scrollImg() {
+  const rect = document.querySelector('.gallery-link').getBoundingClientRect();
+  window.scrollBy({ top: rect.height * 2, left: 0, behavior: 'smooth' });
 }
